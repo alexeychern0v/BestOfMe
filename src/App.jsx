cdb5ca981a6eaff094483c154ecdc7bd26f2e64f
Alexey Chernov <alexchernov063@gmail.com>
2026-06-02 17:38:51 +0200

    Added event handler createNewHabit()

--- src/App.jsx
+++ src/App.jsx
@@ -2,39 +2,49 @@ import HabitCard from './components/HabitCard'
 import waterImage from './assets/waterImage.png'
 import gymImage from './assets/gymImage.jpg'
 import eatImage from './assets/eatImage.jpg'
+import { useState } from 'react'
 
-function HabitsList() {
-  const initialHabits = [{
+
+const initialHabits = [{
     name: 'Drink 2l of water',
     image: waterImage,
     status: 'Done :)',
-    id: 'id1'
+    id: crypto.randomUUID()
   }, {
     name: 'Go to the gym',
     image: gymImage,
     status: 'Not done yet :(',
-    id: 'id2'
+    id: crypto.randomUUID()
   }, {
     name: 'Eat 3000 calories',
     image: eatImage,
     status: 'Not done yet :(',
-    id: 'id3'
+    id: crypto.randomUUID()
   }]
 
+function HabitsList() {
+  const [habits, setHabits] = useState(initialHabits)
+
   function createNewHabit() {
-    console.log('button created')
+    setHabits([
+      ...habits, {
+        name: 'Clean skin',
+        status: 'Not done yet :(',
+        id: crypto.randomUUID()
+      }
+    ])
   }
 
   return (
     <>
-      <button onClick={createNewHabit}>Add habit</button>
-      {initialHabits.map((initialHabit) => {
+      <button onClick={createNewHabit}> Add habit </button>
+      {habits.map((habit) => {
         return (
           <HabitCard
-            name = {initialHabit.name}
-            image = {initialHabit.image}
-            status = {initialHabit.status}
-            key = {initialHabit.id}
+            name = {habit.name}
+            image = {habit.image}
+            status = {habit.status}
+            key = {habit.id}
           />
         )
       })}
